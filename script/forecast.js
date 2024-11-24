c13885aa730e47972d66e77bae07fec9552df25d
fatou fall <fatoufall95@gmail.com>
2024-11-24 10:21:01 -0500

    added bom&dom updated ui

--- script/forecast.js
+++ script/forecast.js
@@ -1,3 +1,4 @@
+// API Key
 const key = "heuyZEYHQiJ8sz4T1xg4bHSy0gEPDrim";
 
 // Get Weather Information
@@ -9,7 +10,7 @@ const getWeather = async (id) => {
   const response = await fetch(baseWeatherUrl + query);
   const data = await response.json();
 
-  return data[0]; // Assuming the response array has the current conditions
+  return data[0]; // Returning the weather for the closest match
 };
 
 // Get City Information
@@ -23,16 +24,3 @@ const getCity = async (city) => {
 
   return data[0]; // Assuming the first result is the desired city
 };
-
-// Fetch city and weather information
-getCity("miami")
-  .then((cityData) => {
-    console.log("City Data:", cityData); // Debugging city data
-    return getWeather(cityData.Key); // Accessing the correct property
-  })
-  .then((weatherData) => {
-    console.log("Weather Data:", weatherData); // Debugging weather data
-  })
-  .catch((err) => console.log("Error:", err));
-
-// getWeather("347936");
--- script/index.js
+++ script/index.js
@@ -1,1 +1,49 @@
+// DOM Elements
+const cityForm = document.querySelector("form");
+const card = document.querySelector(".card");
+const details = document.querySelector(".details");
 
+console.log(details);
+
+// Update the UI
+const updateUI = (data) => {
+  const cityDets = data.cityDets;
+  const weather = data.weather;
+
+  // Update the info on the card
+  details.innerHTML = `
+    <h5 class="my-3">${cityDets.EnglishName}</h5>
+    <div class="my-3">${weather.WeatherText}</div>
+    <div class="display-4 my-4">
+      <span>${weather.Temperature.Imperial.Value}</span>
+      <span>&deg;F</span>
+    </div>
+  `;
+  //remove the d-none class
+
+  if (card.classList.contains("d-none")) {
+    card.classList.remove("d-none");
+  }
+};
+
+// Update City Data
+const updateCity = async (city) => {
+  const cityDets = await getCity(city);
+  const weather = await getWeather(cityDets.Key);
+
+  return { cityDets, weather };
+};
+
+// Event Listener for Form Submit
+cityForm.addEventListener("submit", (evt) => {
+  evt.preventDefault(); // Prevent default form submission behavior
+
+  // Get city value from the form
+  const city = cityForm.city.value.trim();
+  cityForm.reset();
+
+  // Fetch and update the UI with new city data
+  updateCity(city)
+    .then((data) => updateUI(data))
+    .catch((err) => console.log(err));
+});
