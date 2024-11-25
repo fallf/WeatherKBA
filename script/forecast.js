9df5ef47ed23666974262002b77ef1b6804613c4
fatou fall <fatoufall95@gmail.com>
2024-11-25 16:31:18 -0500

    updated the http to https

--- script/forecast.js
+++ script/forecast.js
@@ -2,7 +2,7 @@ const key = "heuyZEYHQiJ8sz4T1xg4bHSy0gEPDrim";
 
 export const getWeather = async (id) => {
   const baseWeatherUrl =
-    "http://dataservice.accuweather.com/currentconditions/v1/";
+    "https://dataservice.accuweather.com/currentconditions/v1/";
   const query = `${id}?apikey=${key}`;
 
   const response = await fetch(baseWeatherUrl + query);
@@ -13,7 +13,7 @@ export const getWeather = async (id) => {
 
 export const getCity = async (city) => {
   const baseCityUrl =
-    "http://dataservice.accuweather.com/locations/v1/cities/search";
+    "https://dataservice.accuweather.com/locations/v1/cities/search";
   const query = `?apikey=${key}&q=${city}`;
 
   const response = await fetch(baseCityUrl + query);
